100f8317e0995dc00da979113449da681eac5333
Natanael <natanael_magalhaes@msn.com>
2021-11-06 08:35:16 -0300

    async image change

--- src/ImageController.js
+++ src/ImageController.js
@@ -19,6 +19,24 @@ class ImageController {
     this.imageElement.src = src
   }
 
+  async updateImageSrcAsync(promise) {
+    // Loading placeholder
+    this.errorElement.classList.add('d-none')
+    this.loadingElement.classList.add('d-none')
+    this.imageElement.classList.remove('d-none')
+
+    try {
+      const url = await promise()
+      this.updateImageSrc(url)
+    } catch {
+      // Error placeholder
+      this.errorElement.classList.remove('d-none')
+      this.loadingElement.classList.add('d-none')
+      this.imageElement.classList.add('d-none')
+      this.imageElement.src = ''
+    }
+  }
+
   _setupListeners() {
     this.imageElement.addEventListener('load', () => {
       this.errorElement.classList.add('d-none')
