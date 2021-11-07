f863423d9b730d53f99573e9248d59cc05362a1b
Natanael <natanael_magalhaes@msn.com>
2021-11-07 10:19:20 -0300

    Aladin Singleton

---
+++ src/core/AladinSingleton.js
@@ -0,0 +1,40 @@
+class AladinSingleton {
+  constructor() {
+    if (AladinSingleton._instance) {
+      return AladinSingleton._instance
+    }
+    AladinSingleton._instance = this
+  }
+
+  init() {
+    try {
+      const hipsUrl = 'https://splus.cloud/HIPS/dr2/color'
+      this.A = window.A.aladin('#aladin-lite-div', {
+        target: '11.4498 -0.8528',
+        fov: 0.14,
+        cooFrame: 'ICRSd',
+        fullScreen: false,
+        showReticle: true,
+        showZoomControl: true,
+        showFullscreenControl: false,
+        showLayersControl: false,
+        showGotoControl: true,
+        showShareControl: false,
+        showSimbadPointerControl: false,
+        showFrame: true,
+        reticleColor: 'rgb(178, 50, 178)',
+        reticleSize: 22
+      })
+      const splusSurvey = this.A.createImageSurvey('S-PLUS', 'S-PLUS', hipsUrl, 'equatorial', 10, { imgFormat: 'png' })
+      this.A.setImageSurvey(splusSurvey)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  setMapRef(ref) {
+    this.mapRef = ref
+  }
+}
+
+export default AladinSingleton
