77aa4fc17ba3401ae702050f1f415ac5ff34f008
Natanael <natanael_magalhaes@msn.com>
2021-11-09 20:42:14 -0300

    include sloan spectra

--- src/components/SpectraTab.js
+++ src/components/SpectraTab.js
@@ -1,8 +1,20 @@
 import ImageContainer from './ImageContainer'
+import SloanService from '../services/SloanService'
 import { useCallback } from 'react'
 
 function SpectraTab({ ra, dec }) {
   const splusUrl = `https://splus-spectra.herokuapp.com/plot?ra=${ra}&dec=${dec}&all`
+
+  const sdssSpecCallback = useCallback(async () => {
+    const sloanService = new SloanService()
+    const id = await sloanService.getSpecId({ ra, dec })
+    if (id) {
+      return `https://skyserver.sdss.org/dr16/en/get/SpecById.ashx?id=${id}`
+    } else {
+      throw new Error('undefined specId')
+    }
+  }, [ra, dec])
+
   return (
     <div>
       {/* Image: S-PLUS PhotoSpectra */}
@@ -10,6 +22,13 @@ function SpectraTab({ ra, dec }) {
         S-PLUS PhotoSpectra
       </div>
       <ImageContainer src={splusUrl} />
+
+
+      {/* Image: SDSS Spectra */}
+      <div className="section-title">
+        SDSS Spectra
+      </div>
+      <ImageContainer srcAsync={sdssSpecCallback} />
 
     </div>
   )
