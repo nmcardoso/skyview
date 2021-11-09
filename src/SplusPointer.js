97a7ad599b66880454d61863057011be476a8446
schwarzam <gustavo.b.schwarz@gmail.com>
2021-11-09 12:06:30 -0300

    slider for brightness

--- src/SplusPointer.js
+++ src/SplusPointer.js
@@ -31,6 +31,27 @@ class SplusPointer {
       this.selectionMode = true
       document.querySelector('canvas.aladin-reticleCanvas').classList.add('splusPointer-cursor')
     })
+
+
+    //Brightness slider 
+    let range = `
+      <div class="sliderContainer">
+        <p id="rangerValue">10</p>
+        <input id="slider" type="range" min="1" max="50" value="10">
+      </div>
+    `
+
+    const splusPointerSlider = createElementFromHTML(range)
+    gotoControl.insertAdjacentElement('afterend', splusPointerSlider)
+
+    splusPointerSlider.addEventListener('input', (e) => {
+      const canvas = document.getElementsByClassName('aladin-imageCanvas')[0]
+      canvas.style.filter = `brightness(${e.target.value})`
+
+      document.getElementById('rangerValue').innerText = e.target.value;
+    })
+    //End slider
+
   }
 
   _setupListeners() {
